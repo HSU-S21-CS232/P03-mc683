e479ddf349ed98df3c42002f1f9dc6dfa08818ad
Mike <mike.greys11@gmail.com>
2021-03-01 12:02:24 -0800

    Defined the class and set volume.

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from PySide2.QtCore import QFile, QObject, QUrl
 from PySide2.QtMultimedia import QMediaPlayer
 
 class MainWindow(QObject):
+    def __init__(self, ui_file, parent=none):
+
+        self.music_player = QMediaPlayer()
+        self.music_player.setVolume(100)
+
+        
