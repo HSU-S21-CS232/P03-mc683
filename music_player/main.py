1daa1ad2cbcfa7a38cf10e1e17b3241fd8665073
Mike <mike.greys11@gmail.com>
2021-03-01 15:05:39 -0800

    Got the ui to work with main.py.

--- music_player/main.py
+++ music_player/main.py
@@ -1,25 +1,31 @@
 import sys
-from PySide2.QtUiTools import QUiLoader
+from PySide2.QtUiTools import QUiLoader #allows us to import .ui files
 from PySide2.QtWidgets import QApplication, QLineEdit, QPushButton, QFileDialog, QAction
 from PySide2.QtCore import QFile, QObject, QUrl
 from PySide2.QtMultimedia import QMediaPlayer
 
 class MainWindow(QObject):
+
+    #class constructor
     def __init__(self, ui_file, parent=None):
 
+        #reference to our music player
         self.music_player = QMediaPlayer()
         self.music_player.setVolume(100)
 
+        #call parent QObject constructor
         super(MainWindow, self).__init__(parent)
 
+        #load the UI file into Python
         ui_file = QFile(ui_file)
         ui_file.open(QFile.ReadOnly)
         loader = QUiLoader()
         self.window = loader.load(ui_file)
 
+        #always remember to close files
         ui_file.close()
 
-
+        #add event listeners
         open_action = self.window.findChild(QAction, 'action_open')
         open_action.triggered.connect(self.open_action_triggered)
 
@@ -29,6 +35,7 @@ class MainWindow(QObject):
         play_button = self.window.findChild(QPushButton, 'play_button')
         play_button.clicked.connect(self.play_button_clicked)
 
+        #show window to user
         self.window.show()
 
     def open_action_triggered(self):
@@ -41,8 +48,7 @@ class MainWindow(QObject):
     def play_button_clicked(self):
         self.music_player.play()
 
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWindow('MainWindow.ui')
+    main_window = MainWindow('MusicPlayer.ui')
     sys.exit(app.exec_())
