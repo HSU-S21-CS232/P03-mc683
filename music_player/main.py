32b4f9784e3bc1690fa7a1451213a0ff621a370c
Mike <mike.greys11@gmail.com>
2021-03-02 19:39:56 -0800

    created the class MainWindow.

--- music_player/main.py
+++ music_player/main.py
@@ -4,3 +4,12 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
+
+from app_modules import *
+
+#I use examples and information from https://www.youtube.com/c/WandersonIsMe/featured . Very helpful and informative.
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
