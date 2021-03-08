8ce4e52dfe10b90763b6b49b34f2e0bb4ca34bbe
Mikey <mike.greys11@gmail.com>
2021-03-08 09:26:00 -0800

    Added class MainWindow.

--- main_window/main.py
+++ main_window/main.py
@@ -1,9 +1,33 @@
 import sys
-import platform
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
-from PySide2.QtWidgets import *
+from PySide2.QtUiTools import QUiLoader
+
+from PySide2.QtWidgets import QApplication, QLineEdit, QPushButton, QMessageBox
+from PySide2.QtCore import QFile, QObject, QIODevice
 
 
 #I use examples and information from https://www.youtube.com/c/WandersonIsMe/featured . Very helpful and informative.
+
+
+class MainWindow(QObject):
+    def __init__(self, ui_file, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        #Load ui file to python.
+        ui_file = QFile(ui_file)
+        if not ui_file.open(QIODevice.ReadOnly):
+            print(f"Cannot open: {ui_file.errorString()}")
+            sys.exit(-1)
+
+
+        ui_file.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+
+        #Be sure to close.
+        ui_file.close()
+
+        #Listeners for UI events.
+        self.addEventListeners()
+
+        #shows window.
+        self.window.show()
